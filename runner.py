d1a9ed6759e6b90e30b42e83afc8c94609df219c
nsbradford <nsbradford@gmail.com>
2017-04-30 17:42:45 -0400

    Outlined future work in TODOs in f_neural(x)

--- runner.py
+++ runner.py
@@ -14,6 +14,10 @@ import matplotlib.pyplot as plt
 # =================================================================================================
 # Globals
 
+# Import MNIST data set
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
 DIMS = 10 # dimensionality of cost function space; equal to number of optimizee params
 scale = tf.random_uniform([DIMS], 0.5, 1.5)
 TRAINING_STEPS = 20 # 100 in the paper ?
@@ -23,34 +27,70 @@ NUM_LAYERS = 2
 STATE_SIZE = 20 # n of hidden units
 
 # =================================================================================================
+# Neural network problem 
 
-def f(x):
+SIZE_H1 = 300
+SIZE_H2 = 40
+SIZE_H3 = 20
+SIZE_OUTPUT = 10
+SIZE_INPUT = 784
+
+LEN_W1 = SIZE_INPUT * SIZE_H1
+LEN_B1 = SIZE_H1
+LEN_W2 = SIZE_H1 * SIZE_H2
+LEN_B2 = SIZE_H2
+LEN_W3 = SIZE_H2 * SIZE_OUTPUT
+LEN_B3 = SIZE_OUTPUT
+
+def f_neural(x):
+    """ This model has ~98.32% accuracy on MNIST.
+        Args:
+            x (tf.Tensor): params of the optimizee network (weights and biases)
+        Returns:
+            (tf.Tensor): the cost (which we are trying to minimize) = 1/ % accurate on train set
+    """
+
+    # TODO need to set these equal to the MNIST training batches
+    NEW_x = tf.placeholder(tf.float32, [None, SIZE_INPUT])
+    y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+    # TODO unpack 'x' into these
+    W_1 = None
+    b_1 = None
+    W_2 = None
+    b_2 = None
+    W_3 = None
+    b_3 = None
+
+    h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+    h2 = tf.nn.relu(tf.matmul(h1, W2) + b2)
+    yhat = tf.matmul(h2, W3) + b3
+
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=yhat))
+    correct_prediction = tf.equal(tf.argmax(yhat,1), tf.argmax(y_,1))
+
+    # TODO this might have to be modified to correctly reside in the TF graph;
+    # we need to replace batch[0] with some large piece of MNIST (the entire train set?) or avg
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy_eval = accuracy.eval(feed_dict={x:batch[0], y_: batch[1]})
+    return 1 / accuracy_eval # we are returning a value to minimize
+
+
+
+# =================================================================================================
+# Multi-dimensional quadratic minimization problem
+
+def f_quadratic(x):
     """ Loss function. Represents the network we are trying to optimize, the "optimizee".
         Could also be thought of as the error landscape, or the "problem" we're solving.
+        Args:
+            x (tf.Tensor): params of the optimizee network (weights and biases)
+        Returns:
+            (tf.Tensor): the cost (which we are trying to minimize)
     """
     x = scale * x
     return tf.reduce_sum(x*x)
 
-
-def learn(optimizer):
-    """ Takes an optimizer function, and applies it in a loop (unroll all the training steps)
-            for number of steps and collects the value of the function f (loss) at each step.
-        Args:
-            optimizer: fcn with arguments (gradients, state)
-        Returns: A list of losses over the training steps
-    """
-    losses = []
-    initial_pos = tf.random_uniform([DIMS], -1., 1.)
-    x = initial_pos
-    state = None
-    for _ in xrange(TRAINING_STEPS):
-        loss = f(x)
-        losses.append(loss)
-        grads, = tf.gradients(loss, x)
-
-        update, state = optimizer(grads, state)
-        x += update
-    return losses
 
 # =================================================================================================
 
@@ -88,6 +128,45 @@ def g_rnn(gradients, state):
     return tf.squeeze(update, axis=[1]), state # No idea what squeeze does...
 
 # =================================================================================================
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def initialize_x():
+    # W_1 = weight_variable([SIZE_INPUT, SIZE_H1])
+    # b_1 = bias_variable([SIZE_H1])
+    # W_2 = weight_variable([SIZE_H1, SIZE_H2])
+    # b_2 = bias_variable([SIZE_H2])
+    # W_3 = weight_variable([SIZE_H2, SIZE_OUTPUT])
+    # b_3 = bias_variable([SIZE_OUTPUT])
+    return tf.random_uniform([DIMS], -1., 1.)
+
+
+def learn(optimizer, problem):
+    """ Takes an optimizer function, and applies it in a loop (unroll all the training steps)
+            for number of steps and collects the value of the function f (loss) at each step.
+        Args:
+            optimizer: fcn with arguments (gradients, state)
+        Returns: A list of losses over the training steps
+    """
+    losses = []
+    x = initialize_x()
+    state = None
+    for _ in xrange(TRAINING_STEPS):
+        loss = problem(x)
+        losses.append(loss)
+        grads, = tf.gradients(loss, x)
+
+        update, state = optimizer(grads, state)
+        x += update
+    return losses
 
 
 def optimize_step(loss):
@@ -157,14 +236,16 @@ def main():
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
     print('Assemble computation graph...')
-    sgd_losses = learn(g_sgd)
-    rms_losses = learn(g_rms)
-    rnn_losses = learn(g_rnn)
+    # TODO change to f_neural
+    problem = f_quadratic
+    sgd_losses = learn(g_sgd, f_quadratic)
+    rms_losses = learn(g_rms, f_quadratic)
+    rnn_losses = learn(g_rnn, f_quadratic)
     sum_losses = tf.reduce_sum(rnn_losses)
     apply_update = optimize_step(sum_losses)
     # display_base_optimizers(sess, loss_list=[sgd_losses, rms_losses], n_times=1)
     train_LSTM(sess, sum_losses, apply_update)
-    # display_LSTM(sess, loss_list=[sgd_losses, rms_losses, rnn_losses], n_times=1)
+    display_LSTM(sess, loss_list=[sgd_losses, rms_losses, rnn_losses], n_times=1)
 
 
 if __name__ == '__main__':
